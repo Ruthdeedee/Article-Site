0d4ce1bdfc514aa3840b0471d777dc0924b3f2f6
Ruthdeedee <113478045+Ruthdeedee@users.noreply.github.com>
2022-12-07 11:01:02 +0100

    new

---
+++ scripts/createnews.js
@@ -0,0 +1,40 @@
+const base_url = "https://61924d4daeab5c0017105f1a.mockapi.io/credo/v1";
+
+const createForm = document.getElementById("createForm");
+const author = document.getElementById("author");
+const title = document.getElementById("title");
+const newsurl = document.getElementById("newsurl");
+const avatarurl = document.getElementById("avatarurl");
+
+const submitNews = async() => {
+    let body = {
+        author:author.value.trim(),
+        title: title.value.trim(),
+        avatar: avatarurl.value.trim(),
+        url: newsurl.value.trim(),
+
+    };
+    try{
+        let response = await fetch(`${base_url}/news`,
+        {
+            method:"POST",
+            body: JSON.stringify(x),
+            headers: {
+                "Content-Type" : "application/json"
+            },
+        });
+
+        let jsonRes = await response.json();
+        console.log(jsonRes);
+        alert("News created successfully");
+        window.location.replace("/");
+    } catch (error) {
+        console.log(error);
+        alert(error);
+}
+};
+
+createForm.addEventListener("submit", function(e){
+e.preventDefault();
+submitNews();
+});
